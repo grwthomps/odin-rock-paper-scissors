40019bfc94f2b6c627657728aa1ebaf037c4536a
Graham Thompson <grwthomps@gmail.com>
2023-02-16 14:25:52 -0600

    Create game function
    This function greets the player and loops through rounds of rock,
    paper, scissors while keeping score. The loop and function end when
    one score reaches 3, followed by a closing message.

--- script.js
+++ script.js
@@ -56,6 +56,62 @@ function playRound(playerSelection, computerSelection) {
     }
   }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    console.log("Hello, Human. Welcome to your daily survival game of Rock, Paper, Scissors. As you know, the rules and stakes are simple. We will each select 'Rock', 'Paper', 'Scissors'. Rock beats Scissors, Paper beats Rock, and Scissors beat Paper. We will play best out of 5 rounds. If you win, you receive the best gift of all: Life! If you lose, you will die. But the good news is you will no longer have to play this game every single day! Let's begin...");
+
+    let computerScore = 0
+    let playerScore = 0
+    let keepGoing = true
+
+    while (keepGoing == true) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result.includes("win")) {
+            playerScore++
+        } else if (result.includes("lose")) {
+            computerScore++
+        } else if (result.includes("tie")) {
+        } else {
+            console.log("Something is wrong with the result of playRound.")
+            keepGoing = false
+        }
+
+        console.log(result);
+        console.log(`Computer: ${computerScore}`)
+        console.log(`You: ${playerScore}`)
+
+        if (playerScore < 3 && computerScore < 3) {
+        } else if (playerScore >= 3 || computerScore >= 3) {
+            if (playerScore < computerScore) {
+                console.log(`Sorry, you have lost. The Computer will now consume you. Goodbye.`);
+                keepGoing = false
+            } else if (playerScore > computerScore) {
+                console.log(`Congratulations, you have won! Your life will be spared...for now.`);
+                keepGoing = false
+            } else if (playerScore == computerScore) {
+                console.log("There has been a tie, which should be impossible. The final score is...");
+                console.log(`Computer: ${computerScore}`);
+                console.log(`You: ${playerScore}`);
+                console.log("Therefore, you have cheated and will be consumed by the Computer. Goodbye.");
+                keepGoing = false
+            } else {
+                console.log("Something is wrong with the scores. The current tally is...");
+                console.log(`Computer: ${computerScore}`);
+                console.log(`You: ${playerScore}`);
+                keepGoing = false
+            }
+        } else {
+            console.log("Something is wrong with the scores. The current tally is...");
+            console.log(`Computer: ${computerScore}`);
+            console.log(`You: ${playerScore}`);
+            keepGoing = false
+        }
+    }
+
+    console.log("To play again, refresh the page.");
+}
+
+game();
